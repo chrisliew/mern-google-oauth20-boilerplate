098f5c027815c577f4fd42b028a7def232cac610
chrisliew <chriswvliew@gmail.com>
2018-12-15 06:37:50 +0700

    added in routes for prod

--- server.js
+++ server.js
@@ -26,6 +26,35 @@ app.use(passport.session());
 
 app.use('/', authRoutes);
 
+// Code for production env
+
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file
+  app.use(express.static('client/build'));
+
+  // Express will serve up index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
+app.all('/*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
